0c550c6c8c4d257fad419b6add0071678b9b4a2e
Shuyi Wang <sdjksdafji@gmail.com>
2017-05-18 02:28:47 -0700

    working

--- src/shallow_cnn.py
+++ src/shallow_cnn.py
@@ -8,11 +8,19 @@ from read_data import get_train_generator, get_test_generator
 # generate_training_testing_split()
 
 model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape=(150, 150, 3)))
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -21,10 +29,10 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(1024))
+model.add(Dense(2048))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(512))
+model.add(Dense(1024))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(144))
@@ -40,7 +48,7 @@ test_generator = get_test_generator()
 model.fit_generator(
     train_generator,
     steps_per_epoch=2000,
-    epochs=50,
+    epochs=100,
     validation_data=test_generator,
     validation_steps=800,
     workers=7)
