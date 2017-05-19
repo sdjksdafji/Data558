cd90c0e540e6af75bc6615fc84cbce21554c9734
Shuyi Wang <sdjksdafji@gmail.com>
2017-05-18 22:13:49 -0700

    finished

--- src/shallow_cnn.py
+++ src/shallow_cnn.py
@@ -1,36 +1,39 @@
-import keras
-from keras.models import Sequential
+from keras.layers import Activation, Flatten, Dense, Dropout
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.models import Sequential
+from keras.optimizers import Adadelta
 
 from read_data import get_train_generator, get_test_generator
 
 # generate_training_testing_split()
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
+
+model.add(Conv2D(64, (3, 3), input_shape=(150, 150, 3), padding="same"))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), padding="same"))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding="same"))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding="same"))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(128, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding="same"))
+model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), padding="same"))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding="same"))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(512, (3, 3)))
+model.add(Conv2D(128, (3, 3), padding="same"))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(2048))
-model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1024))
 model.add(Activation('relu'))
@@ -39,11 +42,13 @@ model.add(Dense(144))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=keras.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0),
+              optimizer=Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0),
               metrics=['categorical_accuracy'])
 
 train_generator = get_train_generator()
 test_generator = get_test_generator()
+
+print(model.summary())
 
 model.fit_generator(
     train_generator,
